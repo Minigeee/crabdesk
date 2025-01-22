2c195d92ed699355159a65b6f7b3eaa64b817283
Tri Nguyen <tri.t.nguyen02@gmail.com>
2025-01-22 12:59:48 -0600

    nuke

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-organize-imports'],
-  singleQuote: true,
-  jsxSingleQuote: true,
-  trailingComma: 'all',
-};
